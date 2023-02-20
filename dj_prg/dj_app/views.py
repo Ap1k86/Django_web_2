8dfe059bd98582897d7bba4042a9f2e6e8cec64f
ap1k86 <ap1k86@yandex.ru>
2023-02-20 19:54:20 +0300

    Переписал страницы с использованием шаблона base.html

--- dj_prg/dj_app/views.py
+++ dj_prg/dj_app/views.py
@@ -32,17 +32,18 @@ def form_processing(request):
     flag = True if request.POST.get("flag") == "on" else False  # Получаем отдельно из формы Флаг
     text = "<h2>Данные из формы:</h2>"  # Переменная содержащая финальный/возвращаемый html
     for key, value in request.POST.items():  # Цикл, который генерирует остальную часть "text"
-        if not key == "csrfmiddlewaretoken" and not key == "flag":  # Исключение двух элементов словаря
-            if key == "country":
-                # 1 способ (хуже)
-                # text += "<b>{}:</b> ".format(key[0].upper() + key[1:])  # Добавляем - Country:
-                # for i in request.POST.getlist("country"):
-                #     text += "{} ".format(i)  # # Добавляем - Англия Германия Испания
-                # text += "<br>"  # Добавляем - Переход на новую строку
-                # 2 способ (лучше)
-                country = ", ".join(request.POST.getlist("country"))  # строка со странами
-                text += "<b>{}:</b> {}<br>".format(key[0].upper() + key[1:], country)  # Добавляем - Country:
-            else:
-                text += "<b>{}:</b> {}<br>".format(key[0].upper() + key[1:], value)
+        if value:
+            if not key == "csrfmiddlewaretoken" and not key == "flag":  # Исключение двух элементов словаря
+                if key == "country":
+                    # 1 способ (хуже)
+                    # text += "<b>{}:</b> ".format(key[0].upper() + key[1:])  # Добавляем - Country:
+                    # for i in request.POST.getlist("country"):
+                    #     text += "{} ".format(i)  # # Добавляем - Англия Германия Испания
+                    # text += "<br>"  # Добавляем - Переход на новую строку
+                    # 2 способ (лучше)
+                    country = ", ".join(request.POST.getlist("country"))  # строка со странами
+                    text += "<b>{}:</b> {}<br>".format(key[0].upper() + key[1:], country)  # Добавляем - Country:
+                else:
+                    text += "<b>{}:</b> {}<br>".format(key[0].upper() + key[1:], value)
     text += "<b>{}:</b> {}<br>".format("Flag", flag)  # Исключение. Добавление отдельно Флага в финальный "text"
     return HttpResponse(text)
